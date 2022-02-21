ab45dd51e8ac4cc5a1ff04d7fbca53a05cb76082
Taquito404 <verobere28@gmail.com>
2022-02-21 11:47:46 -0600

    added ref

--- src/components/AttributionComponent.jsx
+++ src/components/AttributionComponent.jsx
@@ -14,12 +14,18 @@ export default function MusicComponent({ openMusic, handleClosePLotMusic }) {
       <DialogTitle id="scroll-dialog-title">Attributions</DialogTitle>
       <DialogContent>
         <a href="https://www.freepik.es/vectores/marco">
-          Vector de Marco creado por macrovector - www.freepik.es
+          Vector de Marco creado por macrovector
         </a>
         <br />
         <a href="https://www.freepik.es/fotos/fondo">
-          Foto de Fondo creado por vanitjan - www.freepik.es
+          Foto de Fondo creado por vanitjan
         </a>
+        <br />
+        <a href="https://www.rottentomatoes.com/m/spirited_away/reviews?intcmp=rt-what-to-know_read-critics-reviews">
+          Reviews
+        </a>
+        <br />
+        <a href="https://en.wikipedia.org/wiki/Spirited_Away">Wikipedia</a>
       </DialogContent>
     </Dialog>
   );
